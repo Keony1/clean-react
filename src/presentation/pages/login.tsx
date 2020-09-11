adb92d3692f37bf605333c8e919c50f8f47d0ba4
Keony Schröer <keony@edu.univali.br>
2020-09-11 09:27:48 -0300

    refactor: create component for form status

--- src/presentation/pages/login.tsx
+++ src/presentation/pages/login.tsx
@@ -3,7 +3,7 @@ import Styles from './login-styles.scss';
 
 import HeaderLogin from '@/presentation/components/login-header/login-header';
 import Input from '@/presentation/components/input/input';
-import Spinner from '@/presentation/components/spinner/spinner';
+import FormStatus from '../components/form-status/form-status';
 import Footer from '@/presentation/components/footer/footer';
 
 const Login: React.FC = () => {
@@ -16,10 +16,7 @@ const Login: React.FC = () => {
         <Input type="password" name="password" placeholder="Digite sua senha" />
         <button type="submit">Entrar</button>
         <span className={Styles.link}>Criar conta</span>
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner} />
-          <span className={Styles.error}>Erro</span>
-        </div>
+        <FormStatus />
       </form>
       <Footer />
     </div>
