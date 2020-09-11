5a6f92eb47c63d2a8d527dbd126d209e80f99f4c
Keony Schröer <keony@edu.univali.br>
2020-09-11 09:22:31 -0300

    refactor: create component for input

--- src/presentation/pages/login.tsx
+++ src/presentation/pages/login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Styles from './login-styles.scss';
 
 import HeaderLogin from '@/presentation/components/login-header/login-header';
+import Input from '@/presentation/components/input/input';
 import Spinner from '@/presentation/components/spinner/spinner';
 import Footer from '@/presentation/components/footer/footer';
 
@@ -11,19 +12,8 @@ const Login: React.FC = () => {
       <HeaderLogin />
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu e-mail" />
-          <span className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input
-            type="password"
-            name="password"
-            placeholder="Digite sua senha"
-          />
-          <span className={Styles.status}>🔴</span>
-        </div>
-
+        <Input type="email" name="email" placeholder="Digite seu e-mail" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
         <button type="submit">Entrar</button>
         <span className={Styles.link}>Criar conta</span>
         <div className={Styles.errorWrap}>
