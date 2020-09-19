09d33332d88a3b07cece0d77b7f885a7ddd62222
Keony Schröer <keony@edu.univali.br>
2020-09-19 10:07:27 -0300

    fix: move global styles to index

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,7 +1,5 @@
-import React from 'React';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import '@/presentation/styles/global.scss';
 
 import Login from '@/presentation/pages/login/login';
 
