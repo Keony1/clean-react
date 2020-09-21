9b6636f3aa37fb59ee457aad12411c40f48ad3a1
Keony Schröer <keony@edu.univali.br>
2020-09-21 20:57:08 -0300

    test: ensure Login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -61,6 +61,16 @@ describe('Login component', () => {
     expect(passwordStatus.textContent).toBe('🔴');
   });
 
+  test('Should show valid email state if Validation succeeds', () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+    const emailInput = sut.getByTestId('email');
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('tudo certo');
+    expect(emailStatus.textContent).toBe('🔵');
+  });
+
   test('Should show valid password state if Validation succeeds', () => {
     const { sut, validationStub } = makeSut();
     validationStub.errorMessage = null;
